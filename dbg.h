f03f352959660ebc6474bbffc82d5388c27266af
Nick Mosher <nicholastmosher@gmail.com>
2016-02-12 11:13:57 -0500

    Updated c debug macros.

--- dbg.h
+++ dbg.h
@@ -9,17 +9,27 @@
 #include <errno.h>
 #include <string.h>
 
-#ifdef NDEBUG
-#define debug(M, ...)
-#else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#endif
-
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
+//If DOLOG is defined, print log and debug messages.
+#ifdef DOLOG
 #define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 #define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info_or_printf(M, ...) log_info(M, ##__VA_ARGS__)
+#else
+#define log_err(M, ...)
+#define log_warn(M, ...)
+#define log_info(M, ...)
+#define log_info_or_printf(M, ...) printf(M, ##__VA_ARGS__)
+#endif
+
+//If DEBUG is defined, print debug messages.
+#ifdef DEBUG
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug(M, ...)
+#endif
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 #define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
